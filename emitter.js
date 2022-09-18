d68c495653f4fe8ca3a7dfa8adb7dd4995982b9b
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-09-17 21:34:06 -0300

    Started project 01 - NodeJS The Odin Project

--- emitter.js
+++
@@ -1,9 +0,0 @@
-import EventEmitter from "events";
-
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on("st", (x) => {
-  console.log(`started ${x}`);
-});
-
-eventEmitter.emit('st', 'thisAction');
--- url-node.js
+++
@@ -1,9 +0,0 @@
-import { URL } from 'node:url'
-
-const myURL = new URL('https://example.org/foo/bar?baz');
-console.log(myURL.origin);
-console.log(myURL.href);
-
-myURL.href = 'https://example.com/bar';
-console.log(myURL.href);
-// Prints https://example.com/bar
