5e5eb963cd99386655c56881b2f034e36aedb563
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2022-09-21 17:29:14 -0300

    http.createServer notes

--- index.js
+++ index.js
@@ -49,3 +49,4 @@ The callback function logs a message to our console so we can know when the serv
 
 */
 
+/*Let’s break down what happened when we tested our server. Using cURL, we sent a GET request to the server at http://localhost:8000. Our Node.js server listened to connections from that address. The server passed that request to the requestListener() function. The function returned text data with the status code 200. The server then sent that response back to cURL, which displayed the message in our terminal. */
